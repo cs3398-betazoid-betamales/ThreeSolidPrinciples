3506e8839674150877678101d91da224a8ee18e9
CWhitfield91 <cameronrw91@gmail.com>
2019-05-02 20:24:03 -0500

    New New Structure

---
+++ ThreeSolidPrinciples/src/test/java/testthreesolid/TestWorker.java
@@ -0,0 +1,14 @@
+package test.java.testthreesolid;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestWorker {
+    private  Worker w = new Worker();
+    
+    @Test@DisplayName("Work Method Test")
+    public void testWorkMethod(){
+        assertEquals(w.work(),"I'm working already!");
+    }
+}
